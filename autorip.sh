6f790ad3e0e5bc60ba7e4e4921ece8f71ffedcf2
tim <tim.r.hillier@gmail.com>
2019-08-08 18:22:33 -0600

    Initial commit

---
+++ autorip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#close the cd tray
+eject /dev/sr0 -t
+
+#wait for dvd drive to load
+while ! dd if=/dev/sr0 bs=2048 count=1 of=/dev/null 2>/dev/null; do sleep 1; done
+
+#get the name of the dvd
+DVD_NAME="$(blkid -o value -s LABEL /dev/sr0)"
+echo "Dvd Name = $DVD_NAME"
+
+#Rip the DVD
+dvdbackup -i /dev/sr0 -o ../storage/Movies/tmp/ -n $DVD_NAME -t 1
+
+#combine the .VOB files into one file
+cat ../storage/Movies/tmp/$DVD_NAME/VIDEO_TS/*.VOB > ../storage/Movies/tmp/$DVD_NAME/$DVD_NAME.VOB
+
+#transform the vob into a mkv
+ffmpeg -i ../storage/Movies/tmp/$DVD_NAME/$DVD_NAME.VOB -c:v libx264 -preset fast -threads 4 -qp 14 -c:a copy ../storage/Movies/$DVD_NAME.mkv
+
+#get rid of tmp
+rm -rf ../storage/Movies/tmp
+
+#remake tmp
+mkdir ../storage/Movies/tmp
+
+#eject dvd tray
+eject /dev/sr0
